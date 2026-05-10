ca3dc2fce754096855d8c70d6fe28e1971677339
strubium <113206902+strubium@users.noreply.github.com>
2026-05-10 09:44:31 -0500

    Revert "PDF display"
    This reverts commit 0d53e2a328efa96afab14294bfbadc98d0b9d854.

--- src/components/AssetDisplay.js
+++ src/components/AssetDisplay.js
@@ -1,49 +1,21 @@
 import React from 'react';
 import DownloadButton from '@site/src/components/DownloadButton';
 
-const getExtension = (fileType) => {
-  const mimeMap = {
-    'application/pdf': '.pdf',
-    'image/png': '.png',
-    'image/jpeg': '.jpg',
-    'image/jpg': '.jpg',
-    'image/webp': '.webp',
-  };
-
-  return mimeMap[fileType] || '';
-};
-
-const AssetDisplay = ({ imgSrc, name, fileType, downloadUrl }) => {
-  const isPdf = fileType === 'application/pdf';
-
-  return (
+const AssetDisplay = ({imgSrc, name, fileType, downloadUrl}) => (
     <div className="asset-display">
-      {isPdf ? (
-        <iframe
-          src={imgSrc}
-          title={name}
-          width="100%"
-          height="400"
-          style={{ border: '1px solid #ccc' }}
-        />
-      ) : (
         <img
-          src={imgSrc}
-          alt={name}
-          style={{ maxHeight: '10rem' }}
+            src={imgSrc}
+            alt={name}
+            style={{maxHeight: '10rem'}}
         />
-      )}
-
-      <p>Type: {fileType}</p>
-
-      <DownloadButton
-        url={downloadUrl}
-        filename={`${name}${getExtension(fileType)}`}
-      >
-        Download
-      </DownloadButton>
+        <p>Type: {fileType}</p>
+        <DownloadButton
+            url={downloadUrl}
+            filename={`${name}${fileType.split(' ')[1].replace('(', '').replace(')', '')}`}
+        >
+            Download
+        </DownloadButton>
     </div>
-  );
-};
+);
 
 export default AssetDisplay;
